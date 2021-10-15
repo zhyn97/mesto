c7696f5c44cdd071672cd68454b7bd3a82226756
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2021-10-15 21:57:43 +0200

    feature/popuadd add popup add and fix site bug

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,41 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.popup__close');
+const addButton = document.querySelector('.profile__add-button');
+const popup = document.querySelectorAll('.popup');
+const closeButton = document.querySelectorAll('.popup__close');
 const saveButton = document.querySelector('.popup__save-button');
-const form = document.querySelector('.popup__form');
+const formName = document.querySelector('.form-name');
 const nameField = document.querySelector('.profile__name');
 const occupationField = document.querySelector('.profile__occupation');
 const newName = document.querySelector('.popup__name');
 const newOccupation = document.querySelector('.popup__occupation');
+const popupEdit = document.querySelector('.popup-edit');
+const popupAdd = document.querySelector('.popup-add');
 
-function openPopup(){
-    popup.classList.add('popup_active');
+
+
+function openPopup(element){
+    element.classList.add('popup_active');
 }
 
-function closePopup(){
-    popup.classList.remove('popup_active');
+function closePopup() {
+    popup.forEach((el) => {
+      el.classList.remove('popup_active');
+    });
+}
+function editButtonHandler(){
+    openPopup(popupEdit);
+    newName.value = nameField.textContent;
+    newOccupation.value = occupationField.textContent;
+}
+
+function addButtonHandler(){
+    openPopup(popupAdd);
+}
+
+function closeButtonHandler(event){
+    if(event.target.classList.contains('popup__close')){
+        closePopup();
+    }
 }
 
 function savePopup(event){
@@ -31,10 +53,10 @@ function popupCliclHandler(event){
     }
 }
 
-
-newName.value = nameField.textContent;
-newOccupation.value = occupationField.textContent;
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-form.addEventListener('submit', savePopup);
-popup.addEventListener('mouseup', popupCliclHandler);
+editButton.addEventListener('click', editButtonHandler);
+addButton.addEventListener('click', addButtonHandler);
+closeButton.forEach(closeButton =>
+    closeButton.addEventListener('click', closePopup)
+    );    
+formName.addEventListener('submit', savePopup);
+popup.forEach(popup => popup.addEventListener('mouseup',popupCliclHandler));
